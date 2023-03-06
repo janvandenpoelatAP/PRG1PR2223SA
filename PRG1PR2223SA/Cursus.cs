752c1a4428f6d33313fcd8cf1c5f77426e30e32d
janvandenpoel <jan.vandenpoel@vdpc.be>
2023-03-06 18:33:53 +0100

    Opdracht 5: H10-Cursus (oefening)

---
+++ PRG1PR2223SA/Cursus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG1PR2223SA
+{
+    internal class Cursus
+    {
+        public string Titel;
+        public Student[] Studenten = new Student[5];
+
+        public void ToonOverzicht()
+        {
+            Console.WriteLine($"{Titel}");
+            for (int i = 0; i < Studenten.Length; i++)
+            {
+                if (Studenten[i] is not null)
+                {
+                    Console.WriteLine($"{Studenten[i].Naam}");
+                }
+            }
+            Console.WriteLine();
+        }
+        public static void DemonstreerCursussen()
+        {
+            Cursus communicatie = new Cursus();
+            communicatie.Titel = "Communicatie";
+            Cursus programmeren = new Cursus();
+            programmeren.Titel = "Programmeren";
+            Cursus webtechnologie = new Cursus();
+            webtechnologie.Titel = "Webtechnologie";
+            Cursus databanken = new Cursus();
+            databanken.Titel = "Databanken";
+
+            Student student1 = new Student();
+            student1.GeboorteDatum = new DateTime(2001, 1, 3);
+            student1.Naam = "Said Aziz";
+
+
+            Student student2 = new Student();
+            student2.GeboorteDatum = new DateTime(2000, 2, 1);
+            student2.Naam = "Mieke Vermeulen";
+
+            communicatie.Studenten[0] = student1;
+            communicatie.Studenten[1] = student2;
+            programmeren.Studenten[0] = student1;
+            programmeren.Studenten[1] = student2;
+            webtechnologie.Studenten[0] = student1;
+            databanken.Studenten[0] = student2;
+
+            communicatie.ToonOverzicht();
+            programmeren.ToonOverzicht();
+            webtechnologie.ToonOverzicht();
+            databanken.ToonOverzicht();
+        }
+    }
+}
--- PRG1PR2223SA/Program.cs
+++ PRG1PR2223SA/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Student.DemonstreerStudenten();
+            int keuze = 0;
+            Console.WriteLine($"Wat wil je demonstreren?\n\t1. Studenten\n\t2. Cursussen\n");
+            keuze = Convert.ToInt32(Console.ReadLine());
+            if (keuze == 1)
+            {
+                Student.DemonstreerStudenten();
+            }
+            else if (keuze == 2)
+            {
+                Cursus.DemonstreerCursussen();
+            }
         }
     }
 }
