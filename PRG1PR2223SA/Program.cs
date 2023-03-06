d2502aeb0f79df066f09f44f7142e2fde533033f
janvandenpoel <jan.vandenpoel@vdpc.be>
2023-03-06 09:46:45 +0100

    Opdracht 1: Student klasse attributen

--- PRG1PR2223SA/Program.cs
+++ PRG1PR2223SA/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student student1 = new Student();
+            student1.Naam = "Said Aziz";
+            student1.GeboorteDatum = new DateTime(2000, 6, 1);
+            student1.Cursussen = new string[5];
+            student1.Cursussen[0] = "Programmeren";
+            student1.Cursussen[1] = "DataBanken";
+            Student student2 = new Student();
+            student2.Naam = "Mieke Vermeulen";
+            student2.GeboorteDatum = new DateTime(1998, 1, 1);
+            student2.Cursussen = new string[5];
+            student2.Cursussen[0] = "Programmeren";
         }
     }
 }
---
+++ PRG1PR2223SA/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG1PR2223SA
+{
+    internal class Student
+    {
+    }
+}
