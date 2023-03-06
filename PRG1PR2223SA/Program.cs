a170e93e93eebe608988fcb02ef7e159c863cf7a
janvandenpoel <jan.vandenpoel@vdpc.be>
2023-03-06 10:11:25 +0100

    Opdracht 3: Student klasse  access modifiers

--- PRG1PR2223SA/Program.cs
+++ PRG1PR2223SA/Program.cs
@@ -7,14 +7,12 @@
             Student student1 = new Student();
             student1.Naam = "Said Aziz";
             student1.GeboorteDatum = new DateTime(2000, 6, 1);
-            student1.Cursussen = new string[5];
-            student1.Cursussen[0] = "Programmeren";
-            student1.Cursussen[1] = "DataBanken";
+            student1.RegistreerVoorCursus("Programmeren");
+            student1.RegistreerVoorCursus("DataBanken");
             Student student2 = new Student();
             student2.Naam = "Mieke Vermeulen";
             student2.GeboorteDatum = new DateTime(1998, 1, 1);
-            student2.Cursussen = new string[5];
-            student2.Cursussen[0] = "Programmeren";
+            student2.RegistreerVoorCursus("Programmeren");
             Console.WriteLine($"{student1.GenereerNaamkaarje()} => werkbelasting: {student1.BepaalWerkbelasting()}");
             Console.WriteLine($"{student2.GenereerNaamkaarje()} => werkbelasting: {student2.BepaalWerkbelasting()}");
         }
--- PRG1PR2223SA/Student.cs
+++ PRG1PR2223SA/Student.cs
@@ -12,19 +12,45 @@ namespace PRG1PR2223SA
         public string Naam;
         public DateTime GeboorteDatum;
         public uint Sudentennummer;
-        public string[] Cursussen;
+        private string[] cursussen = new string[5];
 
         public string GenereerNaamkaarje()
         {
             return $"{this.Naam} (STUDENT)";
         }
+        // Merk op dat cursussen private is en dus nog enkel vanuit de klasse kan worden aangesproken.
+        public void RegistreerVoorCursus(string cursus)
+        {
+            /*
+            // Quick and dirty met break;
+            for (int i = 0; i < this.cursussen.Length; i++)
+            {
+                if (this.cursussen[i] is null)
+                {
+                    this.cursussen[i] = cursus;
+                    break; // Anders wordt de cursus nog in de volgende lege plaatsen gezet!
+                }
+            }
+            */
+            // Netjes met extra conditie in for loop
+            bool legePlaatsGevonden = false; // wordt true als lege plaats gevonden
+            for (int i = 0; i < cursussen.Length && !legePlaatsGevonden; i++)
+            {
+                if (cursussen[i] is null)
+                {
+                    cursussen[i] = cursus;
+                    legePlaatsGevonden = true;
+                }
+            }
+            // Als legePlaatsGevonden nog false is kan je een foutboodschap schrijven
+        }
         public double BepaalWerkbelasting()
         {
             // Enkel de cursussen meenemen die zijn ingevuld (anders altijd 50!)
             double werkbelasting = 0.0;
-            for (int i = 0; i < this.Cursussen.Length; i++)
+            for (int i = 0; i < this.cursussen.Length; i++)
             {
-                if (Cursussen[i] is not null)
+                if (cursussen[i] is not null)
                 {
                     werkbelasting += 10.0;
                 }
