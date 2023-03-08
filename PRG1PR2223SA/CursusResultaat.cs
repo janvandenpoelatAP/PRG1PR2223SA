890884e2cd6424a628f28de5a6f3d279dde1a574
janvandenpoel <jan.vandenpoel@vdpc.be>
2023-03-08 05:57:17 +0100

    Opdracht 6: H10-CursusResultaat (oefening)

---
+++ PRG1PR2223SA/CursusResultaat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG1PR2223SA
+{
+    internal class CursusResultaat
+    {
+        public string Naam;
+        public byte Resultaat;
+    }
+}
--- PRG1PR2223SA/Student.cs
+++ PRG1PR2223SA/Student.cs
@@ -8,50 +8,23 @@ namespace PRG1PR2223SA
 {
     internal class Student
     {
-        public static uint Sudententeller;
+        public static uint Studententeller;
         public string Naam;
         public DateTime GeboorteDatum;
-        public uint Sudentennummer;
-        private string[] cursussen = new string[5];
-        public byte[] CursusResultaten = new byte[5];
+        public uint Studentennummer;
+        private CursusResultaat[] cursusResultaten = new CursusResultaat[5];
 
         public string GenereerNaamkaarje()
         {
             return $"{this.Naam} (STUDENT)";
         }
-        // Merk op dat cursussen private is en dus nog enkel vanuit de klasse kan worden aangesproken.
-        public void RegistreerVoorCursus(string cursus)
-        {
-            /*
-            // Quick and dirty met break;
-            for (int i = 0; i < this.cursussen.Length; i++)
-            {
-                if (this.cursussen[i] is null)
-                {
-                    this.cursussen[i] = cursus;
-                    break; // Anders wordt de cursus nog in de volgende lege plaatsen gezet!
-                }
-            }
-            */
-            // Netjes met extra conditie in for loop
-            bool legePlaatsGevonden = false; // wordt true als lege plaats gevonden
-            for (int i = 0; i < this.cursussen.Length && !legePlaatsGevonden; i++)
-            {
-                if (this.cursussen[i] is null)
-                {
-                    this.cursussen[i] = cursus;
-                    legePlaatsGevonden = true;
-                }
-            }
-            // Als legePlaatsGevonden nog false is kan je een foutboodschap schrijven
-        }
         public byte BepaalWerkbelasting()
         {
             // Enkel de cursussen meenemen die zijn ingevuld (anders altijd 50!)
             byte werkbelasting = 0;
-            for (int i = 0; i < this.cursussen.Length; i++)
+            for (int i = 0; i < this.cursusResultaten.Length; i++)
             {
-                if (this.cursussen[i] is not null)
+                if (this.cursusResultaten[i] is not null)
                 {
                     werkbelasting += 10;
                 }
@@ -61,7 +34,7 @@ namespace PRG1PR2223SA
         public void Kwoteer(byte cursusIndex, byte behaaldCijfer)
         {
             // Controleer of index voldoet aan voorwaarden
-            if (cursusIndex < 0 || cursusIndex > this.cursussen.Length)
+            if (cursusIndex < 0 || cursusIndex > this.cursusResultaten.Length)
             {
                 Console.WriteLine("Ongeldige cursus index!");
             }
@@ -72,23 +45,38 @@ namespace PRG1PR2223SA
             }
             else
             {
-                this.CursusResultaten[cursusIndex] = behaaldCijfer;
+                this.cursusResultaten[cursusIndex].Resultaat = behaaldCijfer;
             } 
         }
         public double Gemiddelde()
         {
             int aantalCursussen = 0;
             double som = 0.0;
-            for (int i = 0; i < cursussen.Length; i++)
+            for (int i = 0; i < this.cursusResultaten.Length; i++)
             {
-                if (cursussen[i] is not null)
+                if (this.cursusResultaten[i] is not null)
                 {
-                    som += CursusResultaten[i];
+                    som += this.cursusResultaten[i].Resultaat;
                     aantalCursussen++;
                 }
             }
             // Opgelet, als student niet is ingeschreven in een cursus zal programma crashen. Probeer maar.
             return som / aantalCursussen;
+        }
+        public void RegistreerCursusResultaat(string naam, byte resultaat)
+        {
+            bool legePlaatsGevonden = false; // wordt true als lege plaats gevonden
+            for (int i = 0; i < this.cursusResultaten.Length && !legePlaatsGevonden; i++)
+            {
+                if (cursusResultaten[i] is null)
+                {
+                    CursusResultaat nieuwCursusResultaat = new CursusResultaat();
+                    cursusResultaten[i] = nieuwCursusResultaat;
+                    nieuwCursusResultaat.Naam = naam;
+                    Kwoteer((byte)i, resultaat);
+                    legePlaatsGevonden = true;
+                }
+            }
         }
         public void ToonOverzicht()
         {
@@ -102,11 +90,11 @@ namespace PRG1PR2223SA
             Console.WriteLine();
             Console.WriteLine("Cijferrapport");
             Console.WriteLine("*************");
-            for (int i = 0; i < cursussen.Length; i++)
+            for (int i = 0; i < this.cursusResultaten.Length; i++)
             {
-                if (cursussen[i] is not null)
+                if (this.cursusResultaten[i] is not null)
                 {
-                    Console.WriteLine($"{cursussen[i]}:\t{CursusResultaten[i]}");
+                    Console.WriteLine($"{this.cursusResultaten[i].Naam}:\t{this.cursusResultaten[i].Resultaat}");
                 }
             }
             Console.WriteLine($"Gemiddelde:\t{Gemiddelde():F1}\n");
@@ -116,23 +104,17 @@ namespace PRG1PR2223SA
             Student student1 = new Student();
             student1.Naam = "Said Aziz";
             student1.GeboorteDatum = new DateTime(2001, 1, 3);
-            student1.RegistreerVoorCursus("Communicatie");
-            student1.CursusResultaten[0] = 12;
-            student1.RegistreerVoorCursus("Programmeren");
-            student1.CursusResultaten[1] = 15;
-            student1.RegistreerVoorCursus("Webtechnologie");
-            student1.CursusResultaten[2] = 13;
+            student1.RegistreerCursusResultaat("Communicatie", 12);
+            student1.RegistreerCursusResultaat("Programmeren", 15);
+            student1.RegistreerCursusResultaat("Webtechnologie", 13);
             student1.ToonOverzicht();
 
             Student student2 = new Student();
             student2.Naam = "Mieke Vermeulen";
             student2.GeboorteDatum = new DateTime(1998, 1, 1);
-            student2.RegistreerVoorCursus("Communicatie");
-            student2.CursusResultaten[0] = 13;
-            student2.RegistreerVoorCursus("Programmeren");
-            student2.CursusResultaten[1] = 16;
-            student2.RegistreerVoorCursus("Databanken");
-            student2.CursusResultaten[2] = 14;
+            student2.RegistreerCursusResultaat("Communicatie", 13);
+            student2.RegistreerCursusResultaat("Programmeren", 16);
+            student2.RegistreerCursusResultaat("Webtechnologie", 14);
             student2.ToonOverzicht();
         }
     }
