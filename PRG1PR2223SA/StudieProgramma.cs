43b94af64dbcd3e2d6a343547c5cce24c4d1ce0d
janvandenpoel <jan.vandenpoel@vdpc.be>
2023-03-20 08:13:23 +0100

    Opdracht 16: H12-StudieprogrammaStap2

--- PRG1PR2223SA/StudieProgramma.cs
+++ PRG1PR2223SA/StudieProgramma.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolAdmin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -52,9 +53,15 @@ namespace SchoolAdmin
         }
         public static void DemonstreerStudieProgrmma()
         {
+            /* Stap 1
             Cursus communicatie = new Cursus("Communicatie");
             Cursus programmeren = new Cursus("Programmeren");
             Cursus databanken = new Cursus("Databanken", new Student[7], 5);
+            //Cursus[] cursussen = { communicatie, programmeren, databanken };
+            //StudieProgramma programmerenProgramma = new StudieProgramma("Programmeren");
+            //StudieProgramma snbProgramma = new StudieProgramma("Systeem- en netwerkbeheer");
+            //programmerenProgramma.cursussen = cursussen;
+            //snbProgramma.cursussen = cursussen;
             Cursus[] cursussenProgrammeren = { communicatie, programmeren, databanken };
             Cursus[] cursussenSnb = { communicatie, programmeren, databanken };
             StudieProgramma programmerenProgramma = new StudieProgramma("Programmeren");
@@ -63,6 +70,24 @@ namespace SchoolAdmin
             snbProgramma.cursussen = cursussenSnb;
             // later wordt Databanken geschrapt uit het programma SNB
             snbProgramma.cursussen[2] = null;
+            programmerenProgramma.ToonOverzicht();
+            snbProgramma.ToonOverzicht();
+            */
+            Cursus communicatie = new Cursus("Communicatie");
+            Cursus programmeren = new Cursus("Programmeren");
+            Cursus databanken = new Cursus("Databanken", new Student[7], 5);
+            Cursus[] cursussen1 = { communicatie, programmeren, databanken };
+            Cursus[] cursussen2 = { communicatie, programmeren, databanken };
+            StudieProgramma programmerenProgramma = new StudieProgramma("Programmeren");
+            StudieProgramma snbProgramma = new StudieProgramma("Systeem- en netwerkbeheer");
+            programmerenProgramma.cursussen = cursussen1;
+            snbProgramma.cursussen = cursussen2;
+            // later wordt Databanken geschrapt uit het programma SNB
+            // voor SNB wordt bovendien Programmeren hernoemd naar Scripting
+            snbProgramma.cursussen[2] = null;
+            //snbProgramma.cursussen[1].Titel = "Scripting";
+            Cursus scripting = new Cursus("Scripting");
+            snbProgramma.cursussen[1] = scripting; ;
             programmerenProgramma.ToonOverzicht();
             snbProgramma.ToonOverzicht();
         }
