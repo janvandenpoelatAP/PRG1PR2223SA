1092248baeb16cf213db4e7aea11f96b846fa9f6
janvandenpoel <jan.vandenpoel@vdpc.be>
2023-03-30 07:07:28 +0200

    Opdracht 19: H13-AlleStudentenImmutable

--- PRG1PR2223SA/Student.cs
+++ PRG1PR2223SA/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,11 @@ namespace SchoolAdmin
     {
         public static uint Studententeller;
         private static List<Student> alleStudenten = new List<Student>();
-        public static List<Student> AlleStudenten
+        public static ImmutableList<Student> AlleStudenten
         {
             get
             {
-                return alleStudenten;
+                return alleStudenten.ToImmutableList();
             }
         }
         public string Naam;
