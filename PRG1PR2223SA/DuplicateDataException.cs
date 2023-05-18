03cd85782271109c1883207b13391395528668e2
janvandenpoel <jan.vandenpoel@vdpc.be>
2023-05-18 13:29:53 +0200

    Opdracht 33: H16-Geen lege waarden voor vakinschrijving

--- PRG1PR2223SA/DuplicateDataException.cs
+++ PRG1PR2223SA/DuplicateDataException.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace SchoolAdmin
 {
-    internal class DuplicateDataException
+    public class DuplicateDataException : ApplicationException
     {
+        private Object waarde1;
+        public Object Waarde1
+        {
+            get { return waarde1; }
+        }
+        private Object waarde2;
+        public Object Waarde2
+        {
+            get { return waarde2; }
+        }
+        public DuplicateDataException(string message, Object waarde1, Object waarde2) : base(message)
+        {
+            this.waarde1 = waarde1;
+            this.waarde2 = waarde2;
+        }
     }
 }
--- PRG1PR2223SA/VakInschrijving.cs
+++ PRG1PR2223SA/VakInschrijving.cs
@@ -50,10 +50,29 @@ namespace SchoolAdmin
         }
         public VakInschrijving(Student student, Cursus cursus, byte? resultaat)
         {
+            if (student is null || cursus is null)
+            {
+                throw new ArgumentException("Student of cursus mag niet null zijn");
+            }
+            if (ZoekVakInschrijvingOpCursusEnStudent(student, cursus) is not null)
+            {
+                throw new ArgumentException($"Student {student.Naam} is al ingeschreven voor cursus {cursus.Titel}");
+            }
             this.student = student;
             this.cursus = cursus;
             this.Resultaat = resultaat;
             alleVakInschrijvingen.Add(this);
+        }
+        public static VakInschrijving ZoekVakInschrijvingOpCursusEnStudent(Student student, Cursus cursus)
+        {
+            foreach (VakInschrijving vakInschrijving in VakInschrijving.AlleVakInschrijvingen)
+            {
+                if (vakInschrijving.Cursus == cursus && vakInschrijving.Student == student)
+                {
+                    return vakInschrijving;
+                }
+            }
+            return null;
         }
         public static void LeesVanafCommandLine()
         {
@@ -66,19 +85,33 @@ namespace SchoolAdmin
             else
             {
                 Console.WriteLine("Kies een student");
+                Console.WriteLine(0);
+                Console.WriteLine("null");
                 for (int i = 1; i <= Student.AlleStudenten.Count; i++)
                 {
                     Console.WriteLine(i);
                     Console.WriteLine(Student.AlleStudenten[i - 1]);
                 }
-                Student gekozenStudent = Student.AlleStudenten[Convert.ToInt32(Console.ReadLine()) - 1];
+                Student gekozenStudent = null;
+                int keuze = Convert.ToInt32(Console.ReadLine());
+                if (keuze != 0)
+                {
+                    gekozenStudent = Student.AlleStudenten[keuze - 1];
+                }
                 Console.WriteLine("Kies een cursus");
+                Console.WriteLine(0);
+                Console.WriteLine("null");
                 for (int i = 1; i <= Cursus.AlleCursussen.Count; i++)
                 {
                     Console.WriteLine(i);
                     Console.WriteLine(Cursus.AlleCursussen[i - 1].Titel);
                 }
-                Cursus gekozenCursus = Cursus.AlleCursussen[Convert.ToInt32(Console.ReadLine()) - 1];
+                Cursus gekozenCursus = null;
+                keuze = Convert.ToInt32(Console.ReadLine());
+                if (keuze != 0)
+                {
+                    gekozenCursus = Cursus.AlleCursussen[keuze - 1];
+                }
                 Console.WriteLine("Wil je een resultaat toekennen?");
                 byte? resultaat = null;
                 if (Console.ReadLine().ToLower().Trim() == "ja")
@@ -86,7 +119,14 @@ namespace SchoolAdmin
                     Console.WriteLine("Wat is het resultaat?");
                     resultaat = Convert.ToByte(Console.ReadLine());
                 }
-                new VakInschrijving(gekozenStudent, gekozenCursus, resultaat);
+                try
+                {
+                    new VakInschrijving(gekozenStudent, gekozenCursus, resultaat);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
         }
         public static void ToonInschrijvingsGegevens()
